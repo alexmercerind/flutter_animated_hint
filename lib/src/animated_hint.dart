42f8f8c6e13e7da8e6c461bc7ff1ad25eaefdabd
Dipanshu Singh <me@iamdipanshus.in>
2024-10-03 04:49:56 +0530

    implemented animated hint text

---
+++ lib/src/animated_hint.dart
@@ -0,0 +1,420 @@
+import 'dart:ui' as ui;
+
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+/// Type of animation to be used
+enum AnimationType { slide, fade, typer, scale, wavy, flicker }
+
+class AnimatedHintTextField extends StatelessWidget {
+  /// List of hints to be animated
+  final List<String> hints;
+
+  /// type of animation to be used for hints
+  /// by default, [AnimationType.slide] will be used
+  final AnimationType animationType;
+
+  /// This part of hint will remain fixed and will not animate, See example
+  final String? fixedHint;
+
+  /// style for fixed hint text
+  /// if null, default [style] will be used
+  final TextStyle? fixedHintTextStyle;
+
+  /// style for animated hint text
+  /// if null, default [style] will be used
+  final TextStyle? hintTextStyle;
+
+  /// below are the default parameters available in [TextField]
+  final TextMagnifierConfiguration? magnifierConfiguration;
+
+  final Object groupId;
+
+  final TextEditingController? controller;
+
+  final FocusNode? focusNode;
+
+  final InputDecoration? decoration;
+
+  final TextInputType keyboardType;
+
+  final TextInputAction? textInputAction;
+
+  final TextCapitalization textCapitalization;
+
+  final TextStyle? style;
+
+  final StrutStyle? strutStyle;
+
+  final TextAlign textAlign;
+
+  final TextAlignVertical? textAlignVertical;
+
+  final TextDirection? textDirection;
+
+  final bool autofocus;
+
+  final MaterialStatesController? statesController;
+
+  final String obscuringCharacter;
+
+  final bool obscureText;
+
+  final bool autocorrect;
+
+  final SmartDashesType smartDashesType;
+
+  final SmartQuotesType smartQuotesType;
+
+  final bool enableSuggestions;
+
+  final int? maxLines;
+
+  final int? minLines;
+
+  final bool expands;
+
+  final bool readOnly;
+
+  @Deprecated(
+    'Use `contextMenuBuilder` instead. '
+    'This feature was deprecated after v3.3.0-0.5.pre.',
+  )
+  final ToolbarOptions? toolbarOptions;
+
+  final bool? showCursor;
+
+  static const int noMaxLength = -1;
+
+  final int? maxLength;
+
+  final MaxLengthEnforcement? maxLengthEnforcement;
+
+  final ValueChanged<String>? onChanged;
+
+  final VoidCallback? onEditingComplete;
+
+  final ValueChanged<String>? onSubmitted;
+
+  final AppPrivateCommandCallback? onAppPrivateCommand;
+
+  final List<TextInputFormatter>? inputFormatters;
+
+  final bool? enabled;
+
+  final bool? ignorePointers;
+
+  final double cursorWidth;
+
+  final double? cursorHeight;
+
+  final Radius? cursorRadius;
+
+  final bool? cursorOpacityAnimates;
+
+  final Color? cursorColor;
+
+  final Color? cursorErrorColor;
+
+  final ui.BoxHeightStyle selectionHeightStyle;
+
+  final ui.BoxWidthStyle selectionWidthStyle;
+
+  final Brightness? keyboardAppearance;
+
+  final EdgeInsets scrollPadding;
+
+  final bool enableInteractiveSelection;
+
+  final TextSelectionControls? selectionControls;
+
+  final DragStartBehavior dragStartBehavior;
+
+  bool get selectionEnabled => enableInteractiveSelection;
+
+  final GestureTapCallback? onTap;
+
+  final bool onTapAlwaysCalled;
+
+  final TapRegionCallback? onTapOutside;
+
+  final MouseCursor? mouseCursor;
+
+  final InputCounterWidgetBuilder? buildCounter;
+
+  final ScrollPhysics? scrollPhysics;
+
+  final ScrollController? scrollController;
+
+  final Iterable<String>? autofillHints;
+
+  final Clip clipBehavior;
+
+  final String? restorationId;
+
+  final bool scribbleEnabled;
+
+  final bool enableIMEPersonalizedLearning;
+
+  final ContentInsertionConfiguration? contentInsertionConfiguration;
+
+  final EditableTextContextMenuBuilder? contextMenuBuilder;
+
+  final bool canRequestFocus;
+
+  final UndoHistoryController? undoController;
+
+  static Widget _defaultContextMenuBuilder(
+      BuildContext context, EditableTextState editableTextState) {
+    return AdaptiveTextSelectionToolbar.editableText(
+      editableTextState: editableTextState,
+    );
+  }
+
+  final SpellCheckConfiguration? spellCheckConfiguration;
+
+  const AnimatedHintTextField({
+    super.key,
+    required this.hints,
+    this.animationType = AnimationType.slide,
+    this.fixedHint,
+    this.fixedHintTextStyle,
+    this.hintTextStyle,
+    this.groupId = EditableText,
+    this.controller,
+    this.focusNode,
+    this.undoController,
+    this.decoration = const InputDecoration(),
+    TextInputType? keyboardType,
+    this.textInputAction,
+    this.textCapitalization = TextCapitalization.none,
+    this.style,
+    this.strutStyle,
+    this.textAlign = TextAlign.start,
+    this.textAlignVertical,
+    this.textDirection,
+    this.readOnly = false,
+    @Deprecated(
+      'Use `contextMenuBuilder` instead. '
+      'This feature was deprecated after v3.3.0-0.5.pre.',
+    )
+    this.toolbarOptions,
+    this.showCursor,
+    this.autofocus = false,
+    this.statesController,
+    this.obscuringCharacter = '•',
+    this.obscureText = false,
+    this.autocorrect = true,
+    SmartDashesType? smartDashesType,
+    SmartQuotesType? smartQuotesType,
+    this.enableSuggestions = true,
+    this.maxLines = 1,
+    this.minLines,
+    this.expands = false,
+    this.maxLength,
+    this.maxLengthEnforcement,
+    this.onChanged,
+    this.onEditingComplete,
+    this.onSubmitted,
+    this.onAppPrivateCommand,
+    this.inputFormatters,
+    this.enabled,
+    this.ignorePointers,
+    this.cursorWidth = 2.0,
+    this.cursorHeight,
+    this.cursorRadius,
+    this.cursorOpacityAnimates,
+    this.cursorColor,
+    this.cursorErrorColor,
+    this.selectionHeightStyle = ui.BoxHeightStyle.tight,
+    this.selectionWidthStyle = ui.BoxWidthStyle.tight,
+    this.keyboardAppearance,
+    this.scrollPadding = const EdgeInsets.all(20.0),
+    this.dragStartBehavior = DragStartBehavior.start,
+    bool? enableInteractiveSelection,
+    this.selectionControls,
+    this.onTap,
+    this.onTapAlwaysCalled = false,
+    this.onTapOutside,
+    this.mouseCursor,
+    this.buildCounter,
+    this.scrollController,
+    this.scrollPhysics,
+    this.autofillHints = const <String>[],
+    this.contentInsertionConfiguration,
+    this.clipBehavior = Clip.hardEdge,
+    this.restorationId,
+    this.scribbleEnabled = true,
+    this.enableIMEPersonalizedLearning = true,
+    this.contextMenuBuilder = _defaultContextMenuBuilder,
+    this.canRequestFocus = true,
+    this.spellCheckConfiguration,
+    this.magnifierConfiguration,
+  })  : assert(hints.length > 0, "Add at least 1 hint"),
+        assert(obscuringCharacter.length == 1),
+        smartDashesType = smartDashesType ??
+            (obscureText ? SmartDashesType.disabled : SmartDashesType.enabled),
+        smartQuotesType = smartQuotesType ??
+            (obscureText ? SmartQuotesType.disabled : SmartQuotesType.enabled),
+        assert(maxLines == null || maxLines > 0),
+        assert(minLines == null || minLines > 0),
+        assert(
+          (maxLines == null) || (minLines == null) || (maxLines >= minLines),
+          "minLines can't be greater than maxLines",
+        ),
+        assert(
+          !expands || (maxLines == null && minLines == null),
+          'minLines and maxLines must be null when expands is true.',
+        ),
+        assert(!obscureText || maxLines == 1,
+            'Obscured fields cannot be multiline.'),
+        assert(maxLength == null ||
+            maxLength == TextField.noMaxLength ||
+            maxLength > 0),
+        // Assert the following instead of setting it directly to avoid surprising the user by silently changing the value they set.
+        assert(
+          !identical(textInputAction, TextInputAction.newline) ||
+              maxLines == 1 ||
+              !identical(keyboardType, TextInputType.text),
+          'Use keyboardType TextInputType.multiline when using TextInputAction.newline on a multiline TextField.',
+        ),
+        keyboardType = keyboardType ??
+            (maxLines == 1 ? TextInputType.text : TextInputType.multiline),
+        enableInteractiveSelection =
+            enableInteractiveSelection ?? (!readOnly || !obscureText);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      groupId: groupId,
+      controller: controller,
+      focusNode: focusNode,
+      undoController: undoController,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      textCapitalization: textCapitalization,
+      style: style,
+      strutStyle: strutStyle,
+      textAlign: textAlign,
+      textAlignVertical: textAlignVertical,
+      textDirection: textDirection,
+      readOnly: readOnly,
+      toolbarOptions: toolbarOptions,
+      showCursor: showCursor,
+      autofocus: autofocus,
+      statesController: statesController,
+      obscuringCharacter: obscuringCharacter,
+      obscureText: obscureText,
+      autocorrect: autocorrect,
+      smartDashesType: smartDashesType,
+      smartQuotesType: smartQuotesType,
+      enableSuggestions: enableSuggestions,
+      maxLines: maxLines,
+      minLines: minLines,
+      expands: expands,
+      maxLength: maxLength,
+      maxLengthEnforcement: maxLengthEnforcement,
+      onChanged: onChanged,
+      onEditingComplete: onEditingComplete,
+      onSubmitted: onSubmitted,
+      onAppPrivateCommand: onAppPrivateCommand,
+      inputFormatters: inputFormatters,
+      enabled: enabled,
+      ignorePointers: ignorePointers,
+      cursorWidth: cursorWidth,
+      cursorHeight: cursorHeight,
+      cursorRadius: cursorRadius,
+      cursorOpacityAnimates: cursorOpacityAnimates,
+      cursorColor: cursorColor,
+      cursorErrorColor: cursorErrorColor,
+      selectionHeightStyle: selectionHeightStyle,
+      selectionWidthStyle: selectionWidthStyle,
+      keyboardAppearance: keyboardAppearance,
+      scrollPadding: scrollPadding,
+      dragStartBehavior: dragStartBehavior,
+      enableInteractiveSelection: enableInteractiveSelection,
+      selectionControls: selectionControls,
+      onTap: onTap,
+      onTapAlwaysCalled: onTapAlwaysCalled,
+      onTapOutside: onTapOutside,
+      mouseCursor: mouseCursor,
+      buildCounter: buildCounter,
+      scrollController: scrollController,
+      scrollPhysics: scrollPhysics,
+      autofillHints: autofillHints,
+      contentInsertionConfiguration: contentInsertionConfiguration,
+      clipBehavior: clipBehavior,
+      restorationId: restorationId,
+      scribbleEnabled: scribbleEnabled,
+      enableIMEPersonalizedLearning: enableIMEPersonalizedLearning,
+      contextMenuBuilder: contextMenuBuilder,
+      canRequestFocus: canRequestFocus,
+      spellCheckConfiguration: spellCheckConfiguration,
+      magnifierConfiguration: magnifierConfiguration,
+      decoration: (decoration ?? const InputDecoration()).copyWith(
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        floatingLabelAlignment: FloatingLabelAlignment.start,
+        label: Row(
+          children: [
+            if (fixedHint != null)
+              Text(
+                "$fixedHint ",
+                style: fixedHintTextStyle ?? style,
+              ),
+            AnimatedTextKit(
+              repeatForever: true,
+              animatedTexts: _buildAnimatedTexts(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<AnimatedText> _buildAnimatedTexts() {
+    return hints.map((text) {
+      switch (animationType) {
+        case AnimationType.fade:
+          return FadeAnimatedText(
+            text,
+            textStyle: hintTextStyle ?? style,
+            textAlign: TextAlign.start,
+          );
+        case AnimationType.typer:
+          return TyperAnimatedText(
+            text,
+            textStyle: hintTextStyle ?? style,
+            textAlign: TextAlign.start,
+          );
+        case AnimationType.scale:
+          return ScaleAnimatedText(
+            text,
+            textStyle: hintTextStyle ?? style,
+            textAlign: TextAlign.start,
+          );
+        case AnimationType.wavy:
+          return WavyAnimatedText(
+            text,
+            textStyle: hintTextStyle ?? style,
+            textAlign: TextAlign.start,
+          );
+        case AnimationType.flicker:
+          return FlickerAnimatedText(
+            text,
+            textStyle: hintTextStyle ?? style,
+            textAlign: TextAlign.start,
+          );
+        default:
+      }
+
+      return RotateAnimatedText(
+        text,
+        textStyle: hintTextStyle ?? style,
+        alignment: Alignment.centerLeft,
+        textAlign: TextAlign.start,
+      );
+    }).toList();
+  }
+}
